e6cbf1a0a1dd329e1fa8dd70d02fccb73c73d9de
alberto <alberto@cocay.com.mx>
2025-07-25 20:00:49 -0600

    Initial cliente2

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,6 +3,7 @@
 int main(void){
 	// TODO: DEV
 	// Hay que hacer la conexion con servidor
-	std::cout << "Soy el cliente grpc ...." << std::endl;
+	std::cout << "Soy el cliente grpc" << std::endl;
+	std::cout << "Otra prueba mas" << std::endl;
 	return EXIT_SUCCESS;
 }
