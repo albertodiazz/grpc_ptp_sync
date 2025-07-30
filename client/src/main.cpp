c0eb9123545ba6f9dc9e707111e0a9e75943255d
test desarrollo <testdesarrollo.a@gmail.com>
2025-07-29 20:16:21 -0600

    implementacion ptp

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,33 +3,43 @@
 #include <memory>
 #include <grpc/credentials.h>
 #include <grpcpp/grpcpp.h>
-#include "test.grpc.pb.h"
-#include "test.pb.h"
+#include "../proto/test.grpc.pb.h"
+#include <chrono>
 
 class SendRequest : public OffsetService::Service {
 	public:
 		SendRequest(std::shared_ptr<grpc::ChannelInterface> channel)
 			: stub_(OffsetService::NewStub(channel)){}
 
-		double GetTimestamp(){
-			struct timespec ts;
-			clock_gettime(CLOCK_REALTIME, &ts);
-			return ts.tv_sec + static_cast<double>(ts.tv_nsec) / 1e9;
+		double GetTimestamp() {
+			using namespace std::chrono;
+			auto now = system_clock::now();
+			auto duration = now.time_since_epoch();
+
+			auto sec = duration_cast<std::chrono::seconds>(duration).count();
+			auto nsec = duration_cast<std::chrono::nanoseconds>(duration).count() % 1'000'000'000;
+
+			return static_cast<double>(sec) + static_cast<double>(nsec) / 1e9;
 		}
 
 		std::string SendMsg(){
+			double t3 = GetTimestamp();
 			Client request;
-			request.set_t1(GetTimestamp());
+			request.set_t3(t3);
 
 			grpc::ClientContext context;
-			Server server;
-			grpc::Status status = stub_->GetOffset(&context, request, &server);
+			Server response;
+			grpc::Status status = stub_->GetOffset(&context, request, &response);
+			double t2 = GetTimestamp();
 
-			if (status.ok()){
-				std::cout << "Msg recibido timestamp: " << GetTimestamp() << std::endl;
-				return "Todo bien";
-
-			}else {
+			if (status.ok()) {
+				double t1 = response.t1();
+				double t4 = response.t4();
+				double offset = ((t2 - t1) - (t4 - t3)) / 2.0;
+				std::cout << "Offset (PTP): " << offset << " segundos" << std::endl;
+				return "Todo Ok";
+			}
+			else {
 				return status.error_message();
 			}
 		}
@@ -41,7 +51,7 @@ class SendRequest : public OffsetService::Service {
 int main(void){
 	// TODO: DEV
 	// Hay que hacer la conexion con servidor
-	SendRequest cliente(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+	SendRequest cliente(grpc::CreateChannel("10.90.125.35:50051", grpc::InsecureChannelCredentials()));
 	cliente.SendMsg();
 	return EXIT_SUCCESS;
 }
